896b4b539f50cd7d5d549b6e925e05652e62671a
Leehaney gee <georgeleehaney20@gmail.com>
2023-03-03 11:40:35 +0300

    Feat: Add MyList class

---
+++ my_class.rb
@@ -0,0 +1,23 @@
+require_relative './my_enums'
+class MyList
+  include MyEnumerable
+  def initialize(*arr)
+    @list = arr
+  end
+def each
+    @list.each { |i| yield(i) }
+  end
+end
+
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+puts list.all? {|e| e < 5}
+puts list.all? {|e| e > 5}
+
+# Test #any?
+puts list.any? {|e| e == 2}
+puts list.any? {|e| e == 5}
+
+# Test #filter
+puts list.filter {|e| e.even?}
